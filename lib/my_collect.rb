3627f9b923029cd765bed2e5802c6c7b5a4a199c
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-07-10 21:03:24 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
   i = 0
   returned_collection = []
   while i < collection.length
-    new_collection << yield(collection[i])
+    returned_collection << yield(collection[i])
     i += 1
   end
   returned_collection
